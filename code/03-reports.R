6541fae70d070121151a5e6620244cba993c39eb
Sebastian Gibb <mail@sebastiangibb.de>
2021-06-11 15:22:49 +0200

    fix: add targets for missing/corrplot

--- code/03-reports.R
+++ code/03-reports.R
@@ -11,5 +11,31 @@ tg_reports <- list(
            file.path("analysis", "histograms.Rmd")
         },
         format = "file"
+    ),
+    tar_target(
+        report_missing,
+        command = {
+            list(site_conf)
+            !!tar_knitr_deps_expr(file.path("analysis", "missing.Rmd"))
+            workflowr::wflow_build(
+                file.path("analysis", "missing.Rmd"),
+                view = FALSE
+           )
+           file.path("analysis", "missing.Rmd")
+        },
+        format = "file"
+    ),
+    tar_target(
+        report_corrplot,
+        command = {
+            list(site_conf)
+            !!tar_knitr_deps_expr(file.path("analysis", "corrplot.Rmd"))
+            workflowr::wflow_build(
+                file.path("analysis", "corrplot.Rmd"),
+                view = FALSE
+           )
+           file.path("analysis", "corrplot.Rmd")
+        },
+        format = "file"
     )
 )
