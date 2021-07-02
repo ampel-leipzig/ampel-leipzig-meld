7686ce990b003a17b653aaef88ebe6b3365979e8
Sebastian Gibb <mail@sebastiangibb.de>
2021-07-02 10:07:03 +0200

    refactor: remove about page

--- code/99-site.R
+++ code/99-site.R
@@ -3,21 +3,6 @@
 tg_site <- list(
     tar_file(site_conf, file.path("analysis", "_site.yml")),
     tar_target(
-        site_about,
-        command = {
-            list(site_conf)
-            !!tar_knitr_deps_expr(file.path("analysis", "about.Rmd"))
-            # Build
-            workflowr::wflow_build(
-                file.path("analysis", "about.Rmd"),
-                view = FALSE
-            )
-            # Return file name
-            file.path("analysis", "about.Rmd")
-        },
-        format = "file"
-    ),
-    tar_target(
         site_index,
         command = {
             list(site_conf)
