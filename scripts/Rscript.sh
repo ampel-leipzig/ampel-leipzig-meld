4806689c0aeffb7915a7b3091f541fe6e8cf6c63
Sebastian Gibb <mail@sebastiangibb.de>
2021-09-14 10:12:13 +0200

    fix: adapt to new slurm on hpc

--- scripts/Rscript.sh
+++ scripts/Rscript.sh
@@ -3,13 +3,13 @@
 BIND_PATH="/etc/passwd,/etc/group,/var/run"
 
 # on HPC?
-MODULEPATH=/usr/share/Modules/init/bash
-if [ -f "${MODULEPATH}" ] ; then
+MODULESPATH=/usr/share/Modules/init/bash
+if [ -f "${MODULESPATH}" ] ; then
     SINGULARITYVER=3.4.2
-    . "${MODULEPATH}"
-    module load singularity/${SINGULARITYVER} slurm
+    . "${MODULESPATH}"
+    module load singularity/${SINGULARITYVER}
     SINGULARITYPATH=/opt/singularity-${SINGULARITYVER}/bin/singularity
-    BIND_PATH="${BIND_PATH},/etc/slurm"
+    BIND_PATH="${BIND_PATH},/etc/slurm/slurm.conf"
 else # local
     BIND_PATH="${BIND_PATH},${SSH_AUTH_SOCK}"
     SINGULARITYPATH=/usr/bin/singularity
@@ -21,5 +21,6 @@ SINGULARITYENV_LOCPATH=/lib/locale/2.31/ \
     SINGULARITYENV_TAR_MAKE_REPORTER="timestamp" \
     SINGULARITYENV_TZ="UTC" \
     SINGULARITY_BIND="${BIND_PATH}" \
-    ${SINGULARITYPATH} run container/ampel-leipzig-meld.squashfs "${@}"
+    ${SINGULARITYPATH} run container/ampel-leipzig-meld.squashfs \
+    "--vanilla ${@}"
 exit 0
