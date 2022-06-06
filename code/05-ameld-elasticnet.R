baac1e4a7266328ca27afb22a0b727c945bf7006
Sebastian Gibb <mail@sebastiangibb.de>
2022-06-06 21:00:30 +0200

    fix: bootstraping elastic net

--- code/05-ameld-elasticnet.R
+++ code/05-ameld-elasticnet.R
@@ -42,6 +42,28 @@ tg_ameld <- list(
             x = amelddata$x, y = amelddata$y,
             fun = rcv.glmnet,
             family = "cox",
+            nboot = 1000,
+            nfolds = 3,
+            nrepcv = 10,
+            standardize = ameldcfg$standardize,
+            m = ameldcfg$m,
+            times = ameldcfg$times
+        )
+        bootrcv
+    },
+    packages = c(
+        "ameld", "future", "future.batchtools", "glmnet", "survival"
+    ),
+    deployment = "worker"
+    ),
+    tar_target(bootrcv.woICA, {
+        x <- amelddata$x[,
+            !colnames(amelddata$x) %in% c("IL6_S", "CYSC_S", "Age")
+        ]
+        bootrcv <- bootstrap(
+            x = x, y = amelddata$y,
+            fun = rcv.glmnet,
+            family = "cox",
             nboot = ameldcfg$nboot,
             nfolds = ameldcfg$nfolds,
             nrepcv = ameldcfg$nrepcv,
@@ -92,35 +114,65 @@ tg_ameld <- list(
             bootarcv$fit, x = amelddata$x, y = amelddata$y, newx = amelddata$x,
             type = "survival", times = ameldcfg$times, s = "lambda.1se"
         )[1,]
-    })#,
-    #tar_target(bootarcv9, {
-    #    selarcv <- function(...) {
-    #        dots <- list(...)
-    #        a <- arcv.glmnet(...)
-    #        a$models[[
-    #            which.min.error(a, s = dots$s, maxnnzero = dots$maxnnzero)
-    #        ]]
-    #    }
+    }),
+    tar_target(bootarcv7, {
+        selarcv <- function(...) {
+            dots <- list(...)
+            a <- arcv.glmnet(...)
+            a$models[[
+                which.min.error(a, s = "lambda.1se", maxnnzero = dots$maxnnzero)
+            ]]
+        }
 
-    #    bootarcv9 <- bootstrap(
-    #        x = amelddata$x, y = amelddata$y,
-    #        fun = selarcv,
-    #        family = "cox",
-    #        alpha = ameldcfg$alpha,
-    #        s = "lambda.1se",
-    #        maxnnzero = 9,
-    #        nboot = ameldcfg$nboot,
-    #        nfolds = ameldcfg$nfolds,
-    #        nrepcv = ameldcfg$nrepcv,
-    #        standardize = ameldcfg$standardize,
-    #        m = ameldcfg$m,
-    #        times = ameldcfg$times
-    #    )
-    #    bootarcv9
-    #},
-    #packages = c(
-    #    "ameld", "future", "future.batchtools", "glmnet", "survival"
-    #),
-    #deployment = "worker"
-    #)
+        bootarcv7 <- bootstrap(
+            x = amelddata$x, y = amelddata$y,
+            fun = selarcv,
+            family = "cox",
+            alpha = ameldcfg$alpha,
+            s = "lambda.1se",
+            maxnnzero = 7,
+            nboot = ameldcfg$nboot,
+            nfolds = ameldcfg$nfolds,
+            nrepcv = ameldcfg$nrepcv,
+            standardize = ameldcfg$standardize,
+            m = ameldcfg$m,
+            times = ameldcfg$times
+        )
+        bootarcv7
+    },
+    packages = c(
+        "ameld", "future", "future.batchtools", "glmnet", "survival"
+    ),
+    deployment = "worker"
+    ),
+    tar_target(bootarcv9, {
+        selarcv <- function(...) {
+            dots <- list(...)
+            a <- do.call(arcv.glmnet, dots)
+            a$models[[
+                which.min.error(a, s = "lambda.1se", maxnnzero = dots$maxnnzero)
+            ]]
+        }
+
+        bootarcv9 <- bootstrap(
+            x = amelddata$x, y = amelddata$y,
+            fun = selarcv,
+            family = "cox",
+            alpha = ameldcfg$alpha,
+            s = "lambda.1se",
+            maxnnzero = 9,
+            nboot = ameldcfg$nboot,
+            nfolds = ameldcfg$nfolds,
+            nrepcv = ameldcfg$nrepcv,
+            standardize = ameldcfg$standardize,
+            m = ameldcfg$m,
+            times = ameldcfg$times
+        )
+        bootarcv9
+    },
+    packages = c(
+        "ameld", "future", "future.batchtools", "glmnet", "survival"
+    ),
+    deployment = "worker"
+    )
 )
